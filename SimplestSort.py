32fda162b2e64f39fcc05aa9901d30aca7e136b7
theshteves <stevenkneiser@gmail.com>
2022-01-20 15:00:23 -0500

    Patched issue #1: "This is exchange sort" (Credit: @Timeroot)

--- SimplestSort.py
+++ SimplestSort.py
@@ -1,6 +1,19 @@
-def simplest_sort(data: list) -> None:
+def i_cant_believe_it_can_sort(data: list) -> None:
     '''
     "Is this the simplest (and most surprising) sorting algorithm ever?"
+    -- Stanley P. Y. Fung, https://arxiv.org/abs/2110.01111 (Algorithm 1)
+    '''
+
+    N = len(data)
+    for i in range(N):
+        for j in range(N):
+            if data[i] < data[j]:
+                data[i], data[j] = data[j], data[i]
+
+
+def exchange_sort(data: list) -> None:
+    '''
+    Exchange Sort (Algorithm 2 from same paper)
     -- Stanley P. Y. Fung, https://arxiv.org/abs/2110.01111
     '''
 
@@ -8,6 +21,20 @@ def simplest_sort(data: list) -> None:
     for i in range(N):
         for j in range(i + 1, N):
             if data[i] > data[j]:
+                data[i], data[j] = data[j], data[i]
+
+
+def i_cant_believe_it_can_sort_improved(data: list) -> None:
+    '''
+    Algorithm 3, "improved" version of
+    "Is this the simplest (and most surprising) sorting algorithm ever?"
+    -- Stanley P. Y. Fung, https://arxiv.org/abs/2110.01111
+    '''
+
+    N = len(data)
+    for i in range(1, N):
+        for j in range(i):
+            if data[i] < data[j]:
                 data[i], data[j] = data[j], data[i]
 
 
